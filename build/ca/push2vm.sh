f2d6b8594c0279830a227eb69d9878f9ab38344a
caoqiang <caoqiang@jiuqi.com.cn>
2021-09-19 16:18:25 +0800

    up

--- build/ca/push2vm.sh
+++ build/ca/push2vm.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-ADB_HOME=../../platform-tools
-export ADB_HOME #升级为环境变量
+ADB_HOME=$(cd ../../platform-tools;pwd)
 
 function push() {
     # adb 连接到 vmos 虚拟机
-    adb connect $1
+    $ADB_HOME/adb connect $1
 
     # 拷贝文件
-    adb push buildCa/$2 /system/etc/security/cacerts/
+    $ADB_HOME/adb push buildCa/$2 /system/etc/security/cacerts/
 
     # 修改文件权限
-    adb shell <<EOF
+    $ADB_HOME/adb shell <<EOF
     cd /system/etc/security/cacerts/
     chmod 644 $2
     exit
 EOF
-  adb disconnect $1
+  $ADB_HOME/adb disconnect $1
 }
 
 for file in $(ls buildCa/*)
